ce65e168ab8f1f08e7df1ef029d62fb06e73e1a9
proS1996 <sonipronaldo1996@gmail.com>
2025-04-14 14:41:01 +0530

    React Client Setup

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import WebSocketManager from "./components/WebSocketManager";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <WebSocketManager />
+    </div>
+  );
+}
+
+export default App;
